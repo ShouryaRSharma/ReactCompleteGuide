70896cc9f77f2022b7a702aae2de19efa08ea1d5
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2020-09-21 10:54:51 +0100

    using PropTypes for scenarios with other people using components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.scss';
@@ -34,5 +35,12 @@ class Person extends Component {
         )   
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func, 
+    name: PropTypes.string,
+    age: PropTypes.number,
+    change: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
